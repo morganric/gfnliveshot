a064418d1a4c0ddc138c065830b1424c03a632e7
Richard Morgan <morganric@gmail.com>
2013-09-22 08:42:05 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gfnliveshot::Application.config.secret_token = '96163aa5af1eb2aebfac43ab25fc1cf83aab96bbd4a213c8bd51a9a0d6ea6121c6af13964fad5aabd78733e6056211be4e1ca58a84f89b7b560b64101cf1565a'
