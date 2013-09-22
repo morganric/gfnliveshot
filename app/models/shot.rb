392ed9545b692414ab784d4de2c86b5aade96fb8
Richard Morgan <morganric@gmail.com>
2013-09-22 22:57:40 +0100

    config.assets.initialize_on_precompile = false

--- app/models/shot.rb
+++ app/models/shot.rb
@@ -1,6 +1,8 @@
 class Shot < ActiveRecord::Base
   attr_accessible :location, :speak, :user_id, :needed_by
 
+  default_scope order('needed_by ASC')
+
   # accepts_nested_attributes_for :causes
 
   belongs_to :user
